09f35b8b93db90ac722c398942433bfb0bb58fcb
Udo Quattelbaum <u.quattelbaum@outlook.com>
2022-01-26 15:34:43 +0100

    first commit

---
+++ api/src/graphql/posts.sdl.js
@@ -0,0 +1,31 @@
+export const schema = gql`
+  type Post {
+    id: Int!
+    title: String!
+    body: String!
+    createAt: DateTime!
+  }
+
+  type Query {
+    posts: [Post!]! @requireAuth
+    post(id: Int!): Post @requireAuth
+  }
+
+  input CreatePostInput {
+    title: String!
+    body: String!
+    createAt: DateTime!
+  }
+
+  input UpdatePostInput {
+    title: String
+    body: String
+    createAt: DateTime
+  }
+
+  type Mutation {
+    createPost(input: CreatePostInput!): Post! @requireAuth
+    updatePost(id: Int!, input: UpdatePostInput!): Post! @requireAuth
+    deletePost(id: Int!): Post! @requireAuth
+  }
+`
